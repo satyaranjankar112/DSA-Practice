307d94d9f3878a2ef10897eaa3e37334198f6e38
satyaranjankar112 <satyaranjankar2007@gmail.com>
2026-09-09 00:34:40 +0530

    191

---
+++ 191. Number of 1 Bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int c = 0;
+        while (n != 0){
+            int r = n % 2;
+            if (r == 1)
+                c++;
+            n/=2;    
+        }
+        return c;
+    }
+}
